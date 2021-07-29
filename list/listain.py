6fc64fb41b31348586e8aa84e4c6f6555e0b9d1c
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2021-07-29 13:40:27 +0200

    List

---
+++ list/listain.py
@@ -0,0 +1,7 @@
+# in
+# not in 
+# Operacje na listach
+
+imiona = ["Arkadiusz", "Wioletta", "Karol", "Kuba", "Adrian", "Wojtek"]
+liczby = [3, 12, 24, 7, -8]
+
---
+++ list/operacjeifunkcjenalistach.py
@@ -0,0 +1,23 @@
+#len() - długość - length
+#.append - dodać
+#.extend - rozszerzyć
+#.insert(index, co) - wstawić
+#.index - indeks danego el.
+#sort(reverse=False) - sortuj rosnąco
+#max()
+#min()
+#.count - ile razy coś wystąpi
+#.pop - usuń ostatni el.
+#.remove - usuń pierwsze wystąpienie
+#.clear - wyczyść liste
+#.reverse - zamień kolejność
+
+
+lista1 = [54, 1, -2, 20, 1]
+lista2 = ["Arkadiusz", "Wioletta"]
+
+lista1.reverse()
+
+print(lista1)
+
+
