d2f705111c0c7602541f9e35af544edce1a40316
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2021-08-02 10:58:50 +0200

    Screening the equals

--- function/figury.py
+++ function/figury.py
@@ -1,17 +1,23 @@
 import math
 
 def pole_kwadratu(a):
-    return a * a
+    return a * a   
 
 def pole_prostokata(a, b):
-    return a * b
+    return a * b   
 
 def pole_kola(r):
     return math.pi * r ** 2
-
+    
 def pole_trojkata(a, h):
     return 0.5 * a * h
-
+    
 def pole_trapezu(a, b, h):
     return (a + b) / 2 * h
     
+    
+print('Pole koła wynosie: ' +str(pole_kola(2)))
+print('Pole kwadratu wynosie: ' + str(pole_kwadratu(3)))
+print('Pole trójkąta wynosie: ' + str(pole_trojkata(4,5)))
+print('Pole prostokąta wynosie: ' + str(pole_prostokata(3,4)))
+print('Pole trapezu wynosie: ' + str(pole_trapezu(2,2,3)))
