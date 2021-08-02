36d496264261d2c34b06f3760e0b1e3d508fcc99
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2021-08-02 13:38:03 +0200

    Work with Enum

--- function/figury2.py
+++ function/figury2.py
@@ -1,29 +1,32 @@
-import figury
+import figury1
+from enum import IntEnum
 
-choice = input(""" Wybierz figurę, której pole chcesz obliczyć :
+Menu_Figury = IntEnum('Menu_Figury', 'Kwadrat Prostokąt Koło Trójkąt Trapez')
+
+choice = int(input(""" Wybierz figurę, której pole chcesz obliczyć :
 1. Kwadrat
 2. Prostokąt
 3. Koło
 4. Trójkąt
 5. Trapez   
-""")
+"""))
 
-if (choice == '1'):
+if (choice == Menu_Figury.Kwadrat):
     a = float(input('Podaj bok kwadratu:'))
-    print('Pole kwadratu wynosi: '+ str(figury.pole_kwadratu(a)))
-elif (choice == '2'):
+    print('Pole kwadratu wynosi: ', figury1.pole_kwadratu(a))
+elif (choice == Menu_Figury.Prostokąt):
     a = float(input('Podaj pierwszy bok prostokąta:'))
     b = float(input('Podaj drugi bok prostokąta:'))
-    print('Pole prostokąta wynosi: '+ str(figury.pole_prostokata(a,b)))
-elif (choice == '3'):
+    print('Pole prostokąta wynosi: ', figury1.pole_prostokata(a,b))
+elif (choice == Menu_Figury.Koło):
     r = float(input('Podaj promień koła: '))
-    print('Pole koła wynosi: '+ str(figury.pole_kola(r)))
-elif (choice == '4'):
+    print('Pole koła wynosi: ', figury1.pole_kola(r))
+elif (choice == Menu_Figury.Trójkąt):
     a = float(input('Podaj długość podstawy tójkąta: '))
     h = float(input('Podaj wysokość trójkąta:'))
-    print('Pole trójkąta wynosi: '+ str(figury.pole_trojkata(a,h))) 
-elif (choice == '5'):
+    print('Pole trójkąta wynosi: ', figury1.pole_trojkata(a,h)) 
+elif (choice == Menu_Figury.Trapez):
     a = float(input('Podaj długość pierwszej podstawy : '))
-    b = float(input('Podaj długośćć drugij podstawy:'))
+    b = float(input('Podaj długość drugiej podstawy:'))
     h = float(input('Podaj wysokość trapezu:'))
-    print('Pole trapezu wynosi: '+ str(figury.pole_trapezu(a,b,h))) 
+    print('Pole trapezu wynosi: ', figury1.pole_trapezu(a,b,h)) 
